8c5795d8099ca137405b39cc93d72793f44a068b
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-08-15 19:28:59 +0100

    update prodcut card and add content of produtcs

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,15 +1,22 @@
 import Image from 'next/image'
 import React from 'react'
 
-const ProductCard = () => {
+type productType = {
+  category: string
+  title: string
+  image: string
+  description: string
+}
+
+const ProductCard = ({category, title, image, description}: productType) => {
   return (
-    <div className='bg-primary-400 max-w-lg p-4 rounded-2xl shadow-md'>
-      <Image src='/images/image1.jpg' alt='card image' width={500} height={300} className='w-full  rounded-2xl shadow-2xl ' />
-      <div className='text-white flex flex-col gap-2 p-2 mt-3'>
-        <p className='text-sm text-secondary-200 border border-secondary-200 px-3 py-1 rounded-full w-max'>Consultancy</p>
-        <h1 className='text-3xl font-bold leading-15'>Farm Planning</h1>
-        <hr className=' text-white/60' />
-        <p className=' font-neuton text-lg'>Personalized guidance to plan your farm operations and boost efficiency.</p>
+    <div className='bg-primary-600 max-w-lg p-4 rounded-2xl shadow-md h-[500px] flex flex-col gap-5'>
+      <Image src={image} alt='card image' width={500} height={500} className='w-full h-[250px] rounded-2xl shadow-sm object-cover' />
+      <div className='text-white flex flex-col gap-4 p-2 mt-3'>
+        <p className='text-sm text-secondary-200 border border-secondary-200 px-3 py-1 rounded-full w-max'>{category}</p>
+        <h1 className='text-3xl font-bold leading-10'>{title}</h1>
+        <hr className=' text-white/20' />
+        <p className=' font-neuton text-lg'>{description}</p>
       </div>
     </div>
   )
--- sections/Products.tsx
+++ sections/Products.tsx
@@ -3,74 +3,69 @@ import ProductCard from '@/components/ProductCard'
 import React, { useRef } from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-
-const services = [
-  { tag: "Farm Delivery", title: "Product Supplies", image: "/images/product-supplies.jpg" },
-  { tag: "Agricultural Consulting", title: "Agricultural Consulting", image: "/images/agriculture-consulting.jpg" },
-  { tag: "Soil Enhancement", title: "Soil Fertilization", image: "/images/soil-fertilization.jpg" },
-  { tag: "Soil Enhancement", title: "Soil Fertilization", image: "/images/soil-fertilization.jpg" },
-  { tag: "Soil Enhancement", title: "Soil Fertilization", image: "/images/soil-fertilization.jpg" },
-  { tag: "Soil Enhancement", title: "Soil Fertilization", image: "/images/soil-fertilization.jpg" },
-];
+import FadeInSection from '@/components/FadeInSection';
+import { Swiper as SwiperType } from 'swiper';
+import { siteContent } from '../data/content'
 
 const Products = () => {
-  const swiperRef = useRef<any>(null);
+  const swiperRef = useRef<SwiperType | null>(null);
   return (
-    <div className='bg-white w-full h-full container p-8 flex flex-col gap-20 mt-10'>
-      <div>
-        <p className='text-sm text-primary-400 border border-primary-400 px-3 py-2 rounded-full w-max font-semibold '>Products List</p>
-        <div className='flex justify-between'>
-          <h1 className='mt-3 text-2xl sm:text-5xl font-bold font-palanquin'>Smart Products for Smarter Farming</h1>
-          {/* Custom Controls */}
-          <div className="flex justify-center mt-4 gap-4">
-            {/* Prev Button */}
-            <button
-              onClick={() => swiperRef.current?.slidePrev()}
-              className="w-10 h-10 flex items-center justify-center bg-green-700 hover:bg-green-800 text-white rounded-full"
-            >
-              {/* Left Arrow */}
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-              </svg>
-            </button>
+    <FadeInSection>
+      <div className='bg-white w-full h-full container p-8 flex flex-col gap-20 mt-10'>
+        <div>
+          <p className='text-sm text-primary-400 border border-primary-400 px-3 py-2 rounded-full w-max font-semibold '>Products List</p>
+          <div className='flex justify-between'>
+            <h1 className='mt-3 text-2xl sm:text-5xl font-bold font-palanquin'>Smart Products for Smarter Farming</h1>
+            {/* Custom Controls */}
+            <div className="flex justify-center mt-4 gap-2">
+              {/* Prev Button */}
+              <button
+                onClick={() => swiperRef.current?.slidePrev()}
+                className="w-12 h-12 flex items-center justify-center bg-green-700 hover:bg-green-800 text-secondary-200  rounded-full"
+              >
+                {/* Left Arrow */}
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+                </svg>
+              </button>
 
-            {/* Next Button */}
-            <button
-              onClick={() => swiperRef.current?.slideNext()}
-              className="w-10 h-10 flex items-center justify-center bg-green-700 hover:bg-green-800 text-white rounded-full"
-            >
-              {/* Right Arrow */}
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-              </svg>
-            </button>
+              {/* Next Button */}
+              <button
+                onClick={() => swiperRef.current?.slideNext()}
+                className="w-12 h-12 flex items-center justify-center bg-green-700 hover:bg-green-800 text-secondary-200 rounded-full"
+              >
+                {/* Right Arrow */}
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+                </svg>
+              </button>
+            </div>
           </div>
+          <p className='text-primary-400/70 leading-6 text-lg w-2/5 mt-2'>We offer a wide range of agricultural products tailored to meet the needs of farmers at every stage.</p>
+        </div>
+        <div>
+          <Swiper
+            modules={[Navigation]}
+            loop={true}
+            spaceBetween={20}
+            slidesPerView={1}
+            breakpoints={{
+              640: { slidesPerView: 1 }, // >=640px
+              768: { slidesPerView: 2 }, // >=768px
+              1024: { slidesPerView: 3 }, // >=1024px
+            }}
+            onSwiper={(swiper) => (swiperRef.current = swiper)}
+          >
+            {siteContent.products.map((product, i) => (
+              <SwiperSlide key={i}>
+                <ProductCard title={product.title} category={product.category} image={product.image} description={product.description}/>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+
         </div>
       </div>
-      <div>
-        <Swiper
-          modules={[Navigation]}
-          loop={true}
-          spaceBetween={20}
-          slidesPerView={1}
-          breakpoints={{
-            640: { slidesPerView: 1 }, // >=640px
-            768: { slidesPerView: 2 }, // >=768px
-            1024: { slidesPerView: 3 }, // >=1024px
-          }}
-          onSwiper={(swiper) => (swiperRef.current = swiper)}
-        >
-          {services.map((s, i) => (
-            <SwiperSlide key={i}>
-              <ProductCard />
-            </SwiperSlide>
-          ))}
-
-        </Swiper>
-
-
-      </div>
-    </div>
+    </FadeInSection>
   )
 }
 
