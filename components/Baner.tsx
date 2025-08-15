85e10cc98b6449f88e82a3174cbbad925e473b54
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-08-15 19:30:31 +0100

    add some animation and update baner

--- components/Baner.tsx
+++ components/Baner.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import content from '../data/content.json'
+import {siteContent} from '../data/content'
 import Image from 'next/image'
+import FadeInSection from './FadeInSection'
 
 const Baner = () => {
   return (
     <div className='bg-[url(/images/baner.png)] w-full h-[650px] sm:h-[300px] px-4 py-8'>
+      <FadeInSection>
       <div className='container flex flex-col justify-between items-center gap-8 sm:flex-row w-full h-full'>
         <div className='flex flex-col gap-4'>
           <p className='border border-primary-400 rounded-full px-2 py-1 text-sm text-primary-900 w-max'>mosts statistics</p>
           <div className='flex sm:flex-row flex-col gap-4 sm:gap-8'>
-            {content.statistics.map((status, key) => (
+            {siteContent.statistics.map((status, key) => (
               <div className='' key={key}>
                 <p className='text-5xl font-bold font-palanquin'>{status.value}</p>
                 <p className='leading-7 text-sm text-black/50'>
@@ -18,11 +20,13 @@ const Baner = () => {
             ))}
           </div>
         </div>
-        <div className='flex justify-center flex-col gap-2 items-center w-1/5 relative px-8'>
-          <p className='sm:absolute sm:-right-24 right-24 text-primary-400/80 border border-black/50 rounded-full px-2 sm text-2xl font-semibold font-palanquin rotate-0 sm:rotate-90 w-max text-center'>Natural Products</p>
-          <Image src='/images/image4.jpg' alt='baner image' width={500} height={300} className='max-w-80 rounded-2xl shadow-2xl mr-0 sm:mr-22' />
+        <div className='flex justify-center items-center relative gap-5 px-8 w-[500px]'>
+          {/* <p className='sm:absolute sm:-right-24 right-24 text-primary-400/80 border border-black/50 rounded-full px-2 sm text-2xl font-semibold font-palanquin rotate-0 sm:rotate-90 w-max text-center'>Natural Products</p> */}
+           <p className='text-3xl font-semibold'>Natural Products</p>
+          <Image src='/images/image4.jpg' alt='baner image' width={500} height={300} className='max-w-80 rounded-2xl shadow-2xl' />
         </div>
       </div>
+      </FadeInSection>
     </div>
   )
 }
---
+++ components/FadeInSection.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { useEffect, useRef, useState } from "react"
+
+export default function FadeInSection({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const ref = useRef<HTMLDivElement>(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true)
+          observer.unobserve(entry.target) // run only once
+        }
+      },
+      { threshold: 0.1 } // trigger when 10% is visible
+    )
+
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current)
+    }
+  }, [])
+
+  return (
+    <span
+      ref={ref}
+      className={`transition-all duration-1000 ease-out transform ${
+        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8"
+      }`}
+    >
+      {children}
+    </span>
+  )
+}
