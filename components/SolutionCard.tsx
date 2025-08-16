181be3d363e98931d25fbe011776a7928ebd5ed2
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-08-16 21:41:48 +0100

    update responsive design for solution card

--- components/SolutionCard.tsx
+++ components/SolutionCard.tsx
@@ -9,12 +9,12 @@ type solutionCard = {
 
 const SolutionCard = ({ title, description, image }: solutionCard) => {
   return (
-    <div className='group relative  w-80 rounded-2xl shadow-2xl transition-all overflow-hidden'>
+    <div className='group relative w-full rounded-2xl shadow-2xl transition-all overflow-hidden '>
       <Image src={image} alt="image" width={500} height={600} className='rounded-2xl h-[500px] object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out ' />
       <div className='absolute bottom-0 h-5/6 w-full bg-gradient-to-t from-black/90 to-transparent rounded-b-2xl'></div>
-      <div className='absolute -bottom-18 px-4 group-hover:bottom-10 transition-discrete duration-500 ease-in-out' >
+      <div className='absolute bottom-20 px-4 group-hover:bottom-10 transition-discrete duration-500 ease-in-out' >
         <h1 className='text-white text-3xl font-semibold group-hover:text-secondary-200 transition-transform duration-500 ease-in-out'>{title}</h1>
-        <p className='text-white/60 mt-4'>{description}</p>
+        <p className='text-white/60 mt-5'>{description}</p>
       </div>
     </div>
   )
